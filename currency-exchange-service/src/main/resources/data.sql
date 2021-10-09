d54884e36adca46f7719742df1c83c840497222d
nihilo-mike <mikyasmillion851@gmail.com>
2021-10-09 18:09:10 +0800

    commit on main

---
+++ currency-exchange-service/src/main/resources/data.sql
@@ -0,0 +1,9 @@
+INSERT INTO currency_exchange
+(id,currency_from,currency_to,conversion_multiple,enviroment)
+VALUES(10001,'USD','INR',65,'');
+INSERT into currency_exchange
+(id,currency_from,currency_to,conversion_multiple,enviroment)
+VALUES(10002,'EUR','INR',75,'');
+INSERT into currency_exchange
+(id,currency_from,currency_to,conversion_multiple,enviroment)
+VALUES(10003,'AUD','INR',25,'');
