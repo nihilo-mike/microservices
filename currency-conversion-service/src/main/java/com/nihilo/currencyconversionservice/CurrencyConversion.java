352d738b956714224c52706dc2c91281b6174dca
nihilo-mike <mikyasmillion851@gmail.com>
2021-10-12 11:27:38 +0800

    commit on main

---
+++ currency-conversion-service/src/main/java/com/nihilo/currencyconversionservice/CurrencyConversion.java
@@ -0,0 +1,87 @@
+package com.nihilo.currencyconversionservice;
+
+import java.math.BigDecimal;
+
+public class CurrencyConversion {
+    private Long id;
+    private String from;
+    private String to;
+    private BigDecimal quantity;
+    private BigDecimal conversionMultiple;
+    private BigDecimal totalCalculatedAmount;
+    private String   enviroment;
+
+
+    public CurrencyConversion(Long id, String from, String to, BigDecimal quantity, BigDecimal conversionMultiple, BigDecimal totalCalculatedAmount, String enviroment) {
+        this.id = id;
+        this.from = from;
+        this.to = to;
+        this.quantity = quantity;
+        this.conversionMultiple = conversionMultiple;
+        this.totalCalculatedAmount = totalCalculatedAmount;
+        this.enviroment = enviroment;
+    }
+
+    public CurrencyConversion() {
+    }
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFrom() {
+        return this.from;
+    }
+
+    public void setFrom(String from) {
+        this.from = from;
+    }
+
+    public String getTo() {
+        return this.to;
+    }
+
+    public void setTo(String to) {
+        this.to = to;
+    }
+
+    public BigDecimal getQuantity() {
+        return this.quantity;
+    }
+
+    public void setQuantity(BigDecimal quantity) {
+        this.quantity = quantity;
+    }
+
+    public BigDecimal getConversionMultiple() {
+        return this.conversionMultiple;
+    }
+
+    public void setConversionMultiple(BigDecimal conversionMultiple) {
+        this.conversionMultiple = conversionMultiple;
+    }
+
+    public BigDecimal getTotalCalculatedAmount() {
+        return this.totalCalculatedAmount;
+    }
+
+    public void setTotalCalculatedAmount(BigDecimal totalCalculatedAmount) {
+        this.totalCalculatedAmount = totalCalculatedAmount;
+    }
+
+    public String getEnviroment() {
+        return this.enviroment;
+    }
+
+    public void setEnviroment(String enviroment) {
+        this.enviroment = enviroment;
+    }
+
+
+
+ 
+}
