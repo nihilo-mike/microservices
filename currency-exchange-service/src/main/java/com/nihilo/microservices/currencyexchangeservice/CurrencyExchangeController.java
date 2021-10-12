f7a69a88f88afe8c0cb46250eea902530c31eafb
nihilo-mike <mikyasmillion851@gmail.com>
2021-10-12 09:42:27 +0800

    commit on main

--- currency-exchange-service/src/main/java/com/nihilo/microservices/currencyexchangeservice/CurrencyExchangeController.java
+++ currency-exchange-service/src/main/java/com/nihilo/microservices/currencyexchangeservice/CurrencyExchangeController.java
@@ -1,7 +1,5 @@
 package com.nihilo.microservices.currencyexchangeservice;
 
-import java.math.BigDecimal;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,12 +8,21 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CurrencyExchangeController {
-    @Autowired
+   
+   @Autowired
+   private CurrencyExchangeRepository repository;  
+   
+   @Autowired
     private Environment enviroment;
+    
 
  @GetMapping("/currency-exchange/from/{from}/to/{to}")
  public CurrencyExchange retrieveExchangeValue(@PathVariable String from,@PathVariable String to){
-    CurrencyExchange currencyExchange=new CurrencyExchange(1000L,from,to,BigDecimal.valueOf(50)); 
+    CurrencyExchange currencyExchange=repository.findByFromAndTo(from, to);
+    if(currencyExchange==null){
+       throw new RuntimeException
+       ("Unable to find data for"+from+"to"+to);
+    }
    String port=enviroment.getProperty("local.server.port");
     currencyExchange.setEnviroment(port);
   return currencyExchange;
