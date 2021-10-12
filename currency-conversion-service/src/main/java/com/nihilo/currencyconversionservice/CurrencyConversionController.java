a54aef6a7cd1caa0de514cb7c910c1d611148009
nihilo-mike <mikyasmillion851@gmail.com>
2021-10-12 16:18:09 +0800

    commit on main

--- currency-conversion-service/src/main/java/com/nihilo/currencyconversionservice/CurrencyConversionController.java
+++ currency-conversion-service/src/main/java/com/nihilo/currencyconversionservice/CurrencyConversionController.java
@@ -3,6 +3,7 @@ package com.nihilo.currencyconversionservice;
 import java.math.BigDecimal;
 import java.util.HashMap;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,22 +12,17 @@ import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class CurrencyConversionController {
+    @Autowired      
+   private CurrencyExchangeProxy proxy;
 
-    @GetMapping("/currency-conversion/from/{from}/to/{to}/quantity/{quantity}")
-    public CurrencyConversion calculateCurrencyConversion(
+    @GetMapping("/currency-conversion-feign/from/{from}/to/{to}/quantity/{quantity}")
+    public CurrencyConversion calculateCurrencyConversionFeign(
     @PathVariable String from,
     @PathVariable String to,
     @PathVariable BigDecimal quantity){
-      HashMap<String,String>uriVariables=new HashMap<>();
-      uriVariables.put("from",from);
-      uriVariables.put("to", to);
 
-    ResponseEntity<CurrencyConversion> responseEntity= new RestTemplate().getForEntity
-    ("http://localhost:8000/currency-exchange/from/{from}/to/{to}",CurrencyConversion.class,uriVariables);
-    
-    
-    CurrencyConversion currencyConversion= responseEntity.getBody();
-
+      CurrencyConversion currencyConversion=proxy.retrieveExchangeValue(from, to);
+     
       return new CurrencyConversion(currencyConversion.getId(), from, to,
        quantity,currencyConversion.getConversionMultiple(),
        quantity.multiply(currencyConversion.getConversionMultiple()),currencyConversion.getEnviroment()); 
